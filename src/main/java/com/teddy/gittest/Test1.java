7ac693defad9a71311d49ea382870c747279cc14
TeddyH <rusdlsla@gmail.com>
2013-11-27 14:59:15 +0900

    branch feature_x commit

---
+++ src/main/java/com/teddy/gittest/Test1.java
@@ -0,0 +1,19 @@
+package com.teddy.gittest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test1 {
+
+
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		logger.debug("branch feature_x에서 생성한 신규 class");
+
+	}
+
+}
