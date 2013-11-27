25e435afa049458efa90c2d8d1565e08a8ba4367
TeddyH <rusdlsla@gmail.com>
2013-11-27 14:46:35 +0900

    branch test

--- src/main/java/com/teddy/gittest/App.java
+++ src/main/java/com/teddy/gittest/App.java
@@ -1,19 +1,26 @@
 package com.teddy.gittest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 /**
  * Hello world!
  *
  */
-public class App
-{
+public class App {
+
+
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
 	public static void main( String[] args ) {
-		System.out.println( "Hello World!" );
+		logger.debug( "Hello World!" );
 
 
-		for (int i = 0; i < args.length; i++) {
-			System.out.println( "Hello World!" );
+		for (int i = 0; i < 10; i++) {
+			logger.debug( "{} Hello GIT!",i);
 		}
-
 
 	}
 
