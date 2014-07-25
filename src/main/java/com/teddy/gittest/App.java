457e81c8a2128ab893b997239fd31616e15b735f
TeddyH <rusdlsla@gmail.com>
2014-07-25 17:23:39 +0900

    conflict test

--- src/main/java/com/teddy/gittest/App.java
+++ src/main/java/com/teddy/gittest/App.java
@@ -16,6 +16,38 @@ public class App {
 
 	public static void main( String[] args ) {
 		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
+		logger.debug( "Hello World!" );
 
 
 		for (int i = 0; i < 10; i++) {
